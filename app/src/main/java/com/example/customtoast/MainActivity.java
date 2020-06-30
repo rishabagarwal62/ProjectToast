42d34d73daeca3c89add311451f5f7a89588f24c
rishabagarwal62 <rishabagarwal62@gmail.com>
2020-06-30 13:06:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/customtoast/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.customtoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void redPressed(View view){
+
+//        Toast.makeText(this,"OK", Toast.LENGTH_SHORT).show();
+          showCustomToast("Customized Toast");
+    }
+
+    public void bluePressed(View view){
+
+        showCustomToast2("Hey Bro");
+    }
+
+    public void showCustomToast(String string){
+
+        LayoutInflater inflater= getLayoutInflater();
+        View layout= inflater.inflate(R.layout.toast_layout, (ViewGroup) findViewById(R.id.toast_root));
+
+        TextView toastText= layout.findViewById(R.id.toast_text);
+        toastText.setText(string);
+
+        Toast toast= new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER, 0,0);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
+    }
+
+    public void showCustomToast2(String string){
+
+        LayoutInflater inflater= getLayoutInflater();
+        View layout= inflater.inflate(R.layout.toast_layout_bluebutton, (ViewGroup) findViewById(R.id.toast_root2));
+
+        TextView toastText= layout.findViewById(R.id.toast_text2);
+        toastText.setText(string);
+
+        Toast toast= new Toast(getApplicationContext());
+        toast.setGravity(Gravity.BOTTOM, 0,0);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
+
+    }
+}
